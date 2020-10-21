b66169bd4e0c6c1a0c43651708bf19ffa94e74f5
Dina Ayoub <dinaayoub@hotmail.com>
2020-10-21 11:27:34 -0700

    hurray! adding a book now redirects to the book details page

--- server.js
+++ server.js
@@ -20,16 +20,14 @@ app.use(express.static('./public'));
 //connect to the database
 client.connect();
 //if there are any errors, log them
-client.on('error', error => console.error(error));
+client.on('error', error => handleErrors(error));
 
 //handle application routes
 app.get('/', getAllBooks);
 app.get('/books/:id', getBookDetails);
 
 app.get('/searches', (req, res) => {
-  res.render('pages/searches/show', {
-    booksArray: booksArray
-  });
+  res.render('pages/searches/show', { booksArray: booksArray });
 });
 
 app.get('/searches/new', (req, res) => {
@@ -55,6 +53,7 @@ function getBookDetails(req, res) {
     })
     .catch(error => handleErrors(res, error));
 }
+
 function getAllBooks(req, res) {
   let SQL = 'SELECT * FROM books ORDER BY id DESC';
   client.query(SQL)
@@ -65,13 +64,14 @@ function getAllBooks(req, res) {
 }
 
 function saveBook(req, res) {
-  let {index} = req.body;
-  let currentBook=booksArray[index];
-  let SQL = 'INSERT INTO books (image_url,title,author,description,isbn,shelf) VALUES ($1,$2,$3,$4,$5,$6)'
-  let values = [currentBook.image_url,currentBook.title,currentBook.author,currentBook.description,currentBook.isbn, 'fantasy'];
+  let { index } = req.body;
+  let currentBook = booksArray[index];
+  let SQL = 'INSERT INTO books (image_url,title,author,description,isbn,shelf) VALUES ($1,$2,$3,$4,$5,$6) RETURNING id;';
+  let values = [currentBook.image_url, currentBook.title, currentBook.author, currentBook.description, currentBook.isbn, 'fantasy'];
   client.query(SQL, values)
-    .then(() => {
-      getAllBooks(req, res);
+    .then(data => {
+      req.params.id = data.rows[0].id;
+      getBookDetails(req, res);
     })
     .catch(error => handleErrors(res, error));
 }
@@ -86,9 +86,9 @@ function createSearch(req, res) {
   }
   superagent.get(url)
     .then(data => {
-      var i=0;
+      var i = 0;
       data.body.items.map((item) => {
-        booksArray.push(new Book(item,i));
+        booksArray.push(new Book(item, i));
         i++;
       });
       res.render('pages/searches/show', { booksArray: booksArray, page: 'search' });
@@ -96,15 +96,15 @@ function createSearch(req, res) {
     .catch(error => handleErrors(res, error));
 }
 
-function handleErrors(res, error) {
-  console.error(error);
-  res.render('pages/error', { error: error });
+function handleErrors(res, err) {
+  //console.error(error);
+  res.render('pages/error', { error: err });
 }
 app.listen(PORT, () => {
   console.log('server is up at ' + PORT);
 });
 
-function Book(bookData,index) {
+function Book(bookData, index) {
   //check if the volume has imagelinks, and if it does, check if it has a thumbnail before we try to string manipulate
   if (bookData.volumeInfo.imageLinks && bookData.volumeInfo.imageLinks.thumbnail) {
     if (!bookData.volumeInfo.imageLinks.thumbnail.startsWith('https')) {
@@ -119,12 +119,12 @@ function Book(bookData,index) {
   }
   this.title = bookData.volumeInfo.title ? bookData.volumeInfo.title : `Book Title Unknown`;
 
-  this.author = bookData.volumeInfo.authors ? bookData.volumeInfo.authors.reduce((acc,curr) => {
-    acc += curr +', ';
+  this.author = bookData.volumeInfo.authors ? bookData.volumeInfo.authors.reduce((acc, curr) => {
+    acc += curr + ', ';
     return acc;
-  },'' ) : `Book Authors Unknown  `;
+  }, '') : `Book Authors Unknown  `;
   //removing the trailing comma and space
-  this.author = this.author.substring(0,this.author.length - 2);
+  this.author = this.author.substring(0, this.author.length - 2);
 
   this.description = bookData.volumeInfo.description ? bookData.volumeInfo.description : `Book Description Unknown`;
   this.isbn = bookData.volumeInfo.isbn ? bookData.volumeInfo.isbn : `ISBN Unknown`;
